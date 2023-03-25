886207158e8efc325301f540358ffae71b74e6e1
nikhilkarthik <nikhilkarthik241103@gmail.com>
2023-03-25 15:54:17 +0530

    feat: home page in progress

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,29 +1,79 @@
-import React from 'react'
-import Navbar from '../Navbar/Navbar'
-import Drawer from '../Drawer/Drawer'
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+import Drawer from "../Drawer/Drawer";
+import Sidebar from "../home/Sidebar";
+import Cards from "../home/Cards";
 function Home() {
   return (
-    <div className="flex flex-col h-screen">
-    <Navbar/>
-
-    {/* Hero section */}
-    <div className="flex flex-col justify-center items-center h-full bg-gray-100">
-      <h1 className="text-4xl font-bold text-gray-800 mb-4">Welcome to the Organization Management System</h1>
-      <p className="text-lg text-gray-800 mb-8">Manage your organization's employees, departments, and projects with ease.</p>
-      <a href="/createaccount" className="px-4 py-2 bg-indigo-600 text-white rounded-lg hover:bg-indigo-700 transition duration-300 ease-in-out">Get started</a>
-    </div>
-    <Drawer />
-
-    {/* Footer */}
-    <footer className="bg-gray-800 py-4">
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div className="text-center text-gray-400">
-          <p>&copy; 2023 Organization Management System. All rights reserved.</p>
+    <div className="flex h-full w-full bg-slate-900">
+      <nav className="absolute top-0 w-full py-3 sm:px-8 px-3 h-fit flex justify-between">
+        <div className="flex items-center">
+          <img
+            className="h-8 w-auto mr-2"
+            src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+            alt="Workflow"
+          />
+          <h1 className="lg:flex hidden text-white text-xl font-bold">
+            Organization Management System
+          </h1>
+        </div>
+        <div className="relative hidden md:flex gap-6 w-fit">
+          <input className="w-[30rem] h-full pl-10 pr-2 text-slate-300 focus:outline-none bg-slate-900 rounded shadow-md shadow-slate-700" />
+          <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <svg
+              aria-hidden="true"
+              class="w-5 h-5 text-gray-500 dark:text-gray-400"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              ></path>
+            </svg>
+          </div>
+          <button className="h-full flex items-center justify-center aspect-square shadow-md shadow-slate-700">
+            <span className="text-slate-500 peer-focus:text-blue-600 transition-[color pointer-events-none material-symbols-outlined">
+              settings
+            </span>
+          </button>
+          <button className="h-full flex items-center justify-center  aspect-square shadow-md shadow-slate-700">
+            <span className="text-slate-500 peer-focus:text-blue-600 transition-[color pointer-events-none material-symbols-outlined">
+              notifications
+            </span>
+          </button>
+          <button className="h-full flex items-center justify-center  aspect-square shadow-md shadow-slate-700">
+            <span className="text-slate-500 peer-focus:text-blue-600 transition-[color pointer-events-none material-symbols-outlined">
+              person
+            </span>
+          </button>
+        </div>
+      </nav>
+      <Drawer />
+      <Sidebar />
+      <div className="flex gap-8 w-full pl-[150px] pr-10 relative pt-24">
+        <div className="w-full flex-col p-6"></div>
+        <div className="gap-4 flex flex-col">
+          <div className="flex gap-4">
+            <Cards
+              title={"Attendance"}
+              color={"bg-gradient-to-r from-fuchsia-600 to-pink-600"}
+              icon={"clock_loader_60"}
+            />
+            <Cards
+              title={"Performance"}
+              icon={"speed"}
+              color={"bg-gradient-to-r from-orange-400 to-rose-400"}
+            />
+          </div>
         </div>
       </div>
-    </footer>
-  </div>
-  )
+    </div>
+  );
 }
 
-export default Home
+export default Home;
---
+++ client/src/home/Sidebar.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import SidebarComponents from "./SidebarComponents";
+function Sidebar() {
+  const [open, setOpen] = useState(false);
+  const Components = [
+    {
+      name: "Home",
+      icon: "home",
+      type: false,
+    },
+    {
+      name: "Dashboard",
+      icon: "bar_chart_4_bars",
+      type: false,
+    },
+    {
+      name: "Friends",
+      icon: "group",
+      type: false,
+    },
+    {
+      name: "Home",
+      icon: "home",
+      type: false,
+    },
+    {
+      name: "Home",
+      icon: "home",
+      type: false,
+    },
+  ];
+  function open1() {
+    document.getElementById("sidebar").style.left = "0px";
+  }
+  function close() {
+    document.getElementById("sidebar").style.left = "-200px";
+  }
+  return (
+    <div
+      id="sidebar"
+      className="fixed top-[10%] z-10 h-4/5 left-[-200px] w-[300px] flex flex-col gap-8 shadow-md shadow-slate-600 transition-[left] duration-700 rounded-md bg-gray-900 p-4"
+    >
+      {open ? (
+        <div
+          className="flex w-full justify-end cursor-pointer"
+          onClick={() => {
+            setOpen(!open);
+            close();
+          }}
+        >
+          <span className="text-slate-500 text-3xl w peer-focus:text-blue-600 transition-[color pointer-events-none material-symbols-outlined">
+            navigate_before
+          </span>
+        </div>
+      ) : (
+        <div
+          className="flex w-full justify-end cursor-pointer"
+          onClick={() => {
+            setOpen(!open);
+            open1();
+          }}
+        >
+          <span className="text-slate-500 text-3xl peer-focus:text-blue-600 transition-[color pointer-events-none material-symbols-outlined">
+            navigate_next
+          </span>
+        </div>
+      )}
+      {Components.map((e) => (
+        <SidebarComponents name={e.name} icon={e.icon} type={e.type} />
+      ))}
+    </div>
+  );
+}
+
+export default Sidebar;
