b4f785752a35f1e99ede3fc1b02036ceb02d228b
Anand930singh <anand2002king@gmail.com>
2023-03-25 12:02:40 +0530

    Server folder structure is added

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,14 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SignUp from "./Pages/SignUp";
 import Login from "./Pages/Login";
 import CreateAccount from "./Pages/CreateAccount";
+import Attendance from "./Pages/Attendance";
 function App() {
   return (
     <Router>
       <div className='h-screen w-full bg-slate-100'>
         <Routes>
           <Route exact path="/" element={<SignUp/>}></Route>
-          <Route path="/createaccount" element={<CreateAccount/>}></Route>
+          <Route path="/attendance" element={<Attendance/>}></Route>D
         </Routes>
       </div>
     </Router>
